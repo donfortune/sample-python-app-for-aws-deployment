72924f43961d610bbd77d250c2db9ee3af390e89
donfortune <donfortunet.df@gmail.com>
2023-07-03 09:06:46 +0100

    button type changed

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])    #homepage
 def homepage():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        print(first_name)
+
     return render_template('index.html')
 
 
